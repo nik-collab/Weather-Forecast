08c569227d3b7067deccd7d2c86cda08424ebf43
Nikhil kumar Menaria <46754795+Scorphy-designs@users.noreply.github.com>
2019-09-06 13:26:03 +0530

    Add files via upload

---
+++ build/precache-manifest.d825d83cbd5c6023226adcfefb0d3501.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a52257cd562fce480b187b25fea1e093",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6e2f1bbf5752babcdb7",
+    "url": "/static/css/main.bd8e7ee4.chunk.css"
+  },
+  {
+    "revision": "950bfcc3792b7ac9b939",
+    "url": "/static/js/2.c407f00e.chunk.js"
+  },
+  {
+    "revision": "f6e2f1bbf5752babcdb7",
+    "url": "/static/js/main.403257f2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "75ee60b189996fcff9859dd0c6687b66",
+    "url": "/static/media/1.75ee60b1.jpeg"
+  }
+]);
---
+++ src/components/Titles.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Titles = () => (
+	<div>
+		<h1 className="title-container__title">Weather Finder Project </h1>
+		<h3 className="title-container__subtitle">Find out temperature, conditions and more...</h3>
+	    <h3 className="title-container__subtitle"> Made by:-- Nikhil kumar Menaria</h3>
+	</div>
+);
+
+export default Titles;
